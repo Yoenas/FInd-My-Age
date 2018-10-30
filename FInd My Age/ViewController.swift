ce12bad155c5e4e04325d147e74e38f1d41d3466
Yoenas <yoenas@yoenas.local>
2018-10-30 20:35:17 +0700

    Initial Commit

---
+++ FInd My Age/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FInd My Age
+//
+//  Created by Yoenas on 30/10/18.
+//  Copyright © 2018 Yoenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
